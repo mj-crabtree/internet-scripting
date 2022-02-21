23b10bdb786b0ee06c6f925f5b21b27238b4d451
Matt Crabtree <66781848+mj-crabtree@users.noreply.github.com>
2022-02-21 20:52:31 +0000

    crud operations using efcore

--- sections/04/WorkingWithEFCore/Program.cs
+++ sections/04/WorkingWithEFCore/Program.cs
@@ -14,10 +14,28 @@ namespace Crabtree.Shared
         static void Main(string[] args)
         {
             // manipulating data with ef core
-            QueryingCategories();
+            // QueryingCategories();
             // FilteredIncludes();
             // QueryingProducts();
             // QueryingWithLike();
+
+            // if (AddProduct(6, "Bob's Burgers", 500M))
+            // {
+            //     System.Console.WriteLine("Add product successful");
+            // }
+			
+			if (IncreaseProductPrice("Bob", 20M))
+			{
+				System.Console.WriteLine("Price Updated");
+			}
+
+            ListProducts();
+			
+			ReadKey();
+			
+			DeleteProduct("Bob");
+				
+            ListProducts();
         }
 
         static void QueryingCategories()
@@ -36,12 +54,12 @@ namespace Crabtree.Shared
                 //     db.Categories.Include(c => c.Products);
 
                 IQueryable<Category> categories;
-                
+
                 db.ChangeTracker.LazyLoadingEnabled = false;
                 Write("Enable eager loading? Y/N: ");
                 bool eagerLoading = (ReadKey().Key == ConsoleKey.Y);
                 bool explicitLoading = false;
-                
+
                 if (eagerLoading)
                 {
                     categories = db.Categories.Include(c => c.Products);
@@ -61,7 +79,7 @@ namespace Crabtree.Shared
                         Write($"Explicitly load products for {c.CategoryName}? Y/N: ");
                         var key = ReadKey();
                         WriteLine();
-                        
+
                         if (key.Key == ConsoleKey.Y)
                         {
                             var products = db.Entry(c).Collection(c2 => c2.Products);
@@ -148,5 +166,60 @@ namespace Crabtree.Shared
                 }
             }
         }
+
+        static bool AddProduct(int categoriyID, string productName, decimal? price)
+        {
+            using (var db = new Northwind())
+            {
+                var product = new Product()
+                {
+                    CategoryID = categoriyID,
+                    ProductName = productName,
+                    Cost = price
+                };
+                db.Products.Add(product);
+                var affected = db.SaveChanges();
+                return (affected == 1);
+            }
+        }
+
+        static void ListProducts()
+        {
+            using (var db = new Northwind())
+            {
+                WriteLine("{0,-3} {1,-35} {2,8} {3,5} {4}",
+                    "ID", "Product Name", "Cost", "Stock", "Disc.");
+
+                foreach (var item in db.Products.OrderByDescending(p => p.Cost))
+                {
+                    WriteLine("{0:000} {1,-35} {2,8:$#,##0.00} {3,5} {4}",
+                        item.ProductID, item.ProductName, item.Cost,
+                        item.Stock, item.Discontinued);
+                }
+            }
+        }
+
+        static bool IncreaseProductPrice(string name, decimal amount)
+        {
+            using (var db = new Northwind())
+            {
+                var updateProduct = db.Products.First(p => p.ProductName.StartsWith(name));
+				updateProduct.Cost+= amount;
+				int affected = db.SaveChanges();
+				return (affected == 1);
+            }
+        }
+		
+		static bool DeleteProduct(string name)
+		{
+			using (var db = new Northwind())
+			{
+				var deletedProduct = db.Products.Where(p => p.ProductName.StartsWith(name));
+				db.Products.RemoveRange(deletedProduct);
+				int affected = db.SaveChanges();
+				return (affected == 1);
+				
+			}
+		}
     }
 }
