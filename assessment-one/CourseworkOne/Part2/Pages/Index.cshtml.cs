d3f3cdc0aac6ab2802437fc6fe6c148f32624049
Matt Crabtree <66781848+mj-crabtree@users.noreply.github.com>
2022-02-28 15:06:49 +0000

    part 2, exam pass fail needed

---
+++ assessment-one/CourseworkOne/Part2/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Part2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private University context;
+        public IEnumerable<Student> Students { get; set; }
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(University injectedContext, ILogger<IndexModel> logger)
+        {
+            context = injectedContext;
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ViewData["StudentInfo"] = "Matthew Crabtree (B00414581)";
+            Students = context.Students.ToList();
+        }
+    }
+}
